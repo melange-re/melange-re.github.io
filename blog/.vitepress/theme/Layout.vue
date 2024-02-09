75d7a84a8b9a39cb5d98b3890ecae8b8c8f1c04b
Javier Chávarri <javier.chavarri@gmail.com>
2024-02-09 17:15:45 +0100

    fix link to rss

--- blog/.vitepress/theme/Layout.vue
+++ blog/.vitepress/theme/Layout.vue
@@ -29,7 +29,7 @@ const { page, frontmatter } = useData()
           <span class="mr-2 ml-2">·</span>
           <a
             class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="/feed.rss"
+            href="/blog/feed.rss"
             >RSS<span class="hidden sm:inline"> Feed</span></a
           >
           <span class="mr-2 ml-2">·</span>
